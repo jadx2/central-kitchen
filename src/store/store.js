75f62a31d8940d0627adaa2447fc568a65074be7
jadx2 <delmarjaim@gmail.com>
2021-08-20 16:50:02 -0500

    Remove developer tools for redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
@@ -14,16 +14,6 @@ const initialState = {
   error: null,
 };
 
-/* eslint-disable */
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
-/* eslint-enable */
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 export default store;
